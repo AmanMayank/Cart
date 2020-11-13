e0b5fa46f01d3828a1e3e49cf7fda4fc74c3419f
Aman Mayank <aman.msingh87@gmail.com>
2020-11-13 22:31:38 +0530

    Add CartItem styling

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ""
+        }
+    }
     render(){
+        const{price, title, qty} = this.state;
         return(
            <div className="cart-item">
                <div className="left-block">   
                     <img style={styles.image} alt = "Phone"/> 
                </div>
                <div className="right-block">
-                   <div style ={{fontSize:25}}>Phone</div>
-                   <div>RS 999</div>
-                   <div>Qty:1</div>
+                   <div style ={{fontSize:25}}>{title}</div>
+                   <div>Rs {price}</div>
+                   <div>Qty: {qty}</div>
                    <div className="cart-item-actions">
                     {/*Buttons*/}
+                        <img alt="increase" className="action-icons" src= "https://www.flaticon.com/svg/static/icons/svg/992/992651.svg"/>
+                        <img alt="decrease" className="action-icons" src= "https://www.flaticon.com/svg/static/icons/svg/659/659892.svg"/>
+                        <img alt="delete" className="action-icons" src= "https://www.flaticon.com/svg/static/icons/svg/1345/1345823.svg"/> 
                    </div>
                </div>
            </div> 
