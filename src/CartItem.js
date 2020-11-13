b8d134b2c6fb1482874fb89ed6bdd101dd72d89d
Aman Mayank <aman.msingh87@gmail.com>
2020-11-13 22:59:03 +0530

    Removed bugs

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,6 +10,10 @@ class CartItem extends React.Component{
             img: ""
         }
     }
+
+    increaseQuantity = () => {
+        console.log('this.state', this.state);
+    }
     render(){
         const{price, title, qty} = this.state;
         return(
@@ -23,9 +27,22 @@ class CartItem extends React.Component{
                    <div>Qty: {qty}</div>
                    <div className="cart-item-actions">
                     {/*Buttons*/}
-                        <img alt="increase" className="action-icons" src= "https://www.flaticon.com/svg/static/icons/svg/992/992651.svg"/>
-                        <img alt="decrease" className="action-icons" src= "https://www.flaticon.com/svg/static/icons/svg/659/659892.svg"/>
-                        <img alt="delete" className="action-icons" src= "https://www.flaticon.com/svg/static/icons/svg/1345/1345823.svg"/> 
+                        <img 
+                        alt="increase" 
+                        className="action-icons" 
+                        src= "https://www.flaticon.com/svg/static/icons/svg/992/992651.svg"
+                        onClick = {this.increaseQuantity}
+                        />
+                        <img
+                        alt="decrease" 
+                        className="action-icons" 
+                        src= "https://www.flaticon.com/svg/static/icons/svg/659/659892.svg"
+                        />
+                        <img 
+                        alt="delete" 
+                        className="action-icons" 
+                        src= "https://www.flaticon.com/svg/static/icons/svg/1345/1345823.svg"
+                        /> 
                    </div>
                </div>
            </div> 
